399de36db98940748fc3d686d5090ecd7f63139d
358067067 <xxhundan@qq.com>
2021-01-11 20:54:36 +0800

    实现查询

--- src/webapp/model/user.go
+++ src/webapp/model/user.go
@@ -13,7 +13,7 @@ type User struct {
 	Email string
 }
 
-//插入User数据
+//AddUser 插入User数据
 func (user *User) AddUser() error {
 	// sql语句
 	sqlStr := "insert into users(name, pwd, email) values(?,?,?)"
@@ -24,11 +24,64 @@ func (user *User) AddUser() error {
 		return err
 	}
 	// 执行插入语句
-	_, err2 := inStmt.Exec("admin", "123456", "xxhundan@qq.com")
-	// _, err2 := inStmt.Exec(sqlStr, "admin", "123456", "xxhundan@qq.com")
+	_, err2 := inStmt.Exec(sqlStr, user.Name, user.Pwd, user.Email)
 	if err2 != nil {
 		fmt.Println("新增用户失败", err2)
 		return err2
 	}
 	return nil
+}
+
+//GetUserByName 查询User数据
+func (user *User) GetUserByName() (*User, error) {
+	sqlStr := "select id, name, pwd ,email from users where name = ?"
+	row := utils.Db.QueryRow(sqlStr, user.Name)
+	// 变量赋值
+	var id int
+	var name string
+	var pwd string
+	var email string
+	err := row.Scan(&id, &name, &pwd, &email)
+	if err != nil {
+		fmt.Println("查询异常", err)
+		return nil, err
+	}
+	u := &User{
+		ID:    id,
+		Name:  name,
+		Pwd:   pwd,
+		Email: email,
+	}
+	return u, nil
+}
+
+//GetUsers 查询所有用户
+func (user *User) GetUsers() ([]*User, error) {
+	sqlStr := "select id, name, pwd, email from users"
+	rows, err := utils.Db.Query(sqlStr)
+	if err != nil {
+		fmt.Println("查询异常", err)
+		return nil, err
+	}
+	var users []*User
+	for rows.Next() {
+		// 变量赋值
+		var id int
+		var name string
+		var pwd string
+		var email string
+		err := rows.Scan(&id, &name, &pwd, &email)
+		if err != nil {
+			fmt.Println("查询异常", err)
+			return nil, err
+		}
+		u := &User{
+			ID:    id,
+			Name:  name,
+			Pwd:   pwd,
+			Email: email,
+		}
+		users = append(users, u)
+	}
+	return users, nil
 }
--- src/webapp/model/user_test.go
+++ src/webapp/model/user_test.go
@@ -6,7 +6,17 @@ import (
 )
 
 func TestUser(t *testing.T) {
-	fmt.Println("新增用户测试")
-	user := &User{}
-	user.AddUser()
+	// fmt.Println("新增用户测试")
+	// user := User{
+	// Name: "admin",
+	// }
+	// user.AddUser()
+	// fmt.Println("查询用户测试")
+	// u, _ := user.GetUserByName()
+	// fmt.Println(u)
+	u := User{}
+	users, _ := u.GetUsers()
+	for k, v := range users {
+		fmt.Printf("%v--------%v\n", k+1, v)
+	}
 }
